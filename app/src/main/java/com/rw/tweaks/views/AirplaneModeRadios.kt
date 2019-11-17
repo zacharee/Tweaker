b0f4cd321ef1202a6c6d104f02d05b8398bcdbd7
zacharee <zachary.wander@gmail.com>
2019-11-17 07:37:13 -0500

    Redesign Airplane Mode Tweaks UI

--- app/src/main/java/com/rw/tweaks/views/AirplaneModeRadios.kt
+++ app/src/main/java/com/rw/tweaks/views/AirplaneModeRadios.kt
@@ -2,13 +2,18 @@ package com.rw.tweaks.views
 
 import android.content.Context
 import android.provider.Settings
-import android.text.TextUtils
 import android.util.AttributeSet
-import android.widget.FrameLayout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.rw.tweaks.R
 import com.rw.tweaks.util.writeGlobal
+import kotlinx.android.synthetic.main.airplane_mode_radio.view.*
 import kotlinx.android.synthetic.main.airplane_mode_radios.view.*
 
-class AirplaneModeRadios(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
+class AirplaneModeRadios(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
     companion object {
         const val CELL = "cell"
         const val BT = "bluetooth"
@@ -16,119 +21,102 @@ class AirplaneModeRadios(context: Context, attrs: AttributeSet) : FrameLayout(co
         const val NFC = "nfc"
         const val WMX = "wimax"
     }
+
+    data class RadioInfo(
+        val name: Int,
+        val id: String,
+        var isExempt: Boolean,
+        var isToggleable: Boolean
+    )
     
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
         val currentBlacklisted = (Settings.Global.getString(context.contentResolver, Settings.Global.AIRPLANE_MODE_RADIOS) ?: "").split(",")
         val currentToggleable = (Settings.Global.getString(context.contentResolver, Settings.Global.AIRPLANE_MODE_TOGGLEABLE_RADIOS) ?: "").split(",")
-        
-        val radioCell = radio_cell
-        val radioBt = radio_bt
-        val radioWifi = radio_wifi
-        val radioNfc = radio_nfc
-        val radioWmx = radio_wimax
-        
-        val toggleCell = toggle_cell
-        val toggleBt = toggle_bluetooth
-        val toggleWifi = toggle_wifi
-        val toggleNfc = toggle_nfc
-        val toggleWmx = toggle_wimax
 
-        radioCell.apply { 
-            isChecked = !currentBlacklisted.contains(CELL)
-            setOnCheckedChangeListener { _, isChecked -> 
-                updateBlacklist(isChecked, radioBt.isChecked, radioWifi.isChecked, radioNfc.isChecked, radioWmx.isChecked)
-            }
-        }
+        val items = arrayListOf(
+            RadioInfo(
+                R.string.option_airplane_mode_radio_cell,
+                CELL,
+                !currentBlacklisted.contains(CELL),
+                currentToggleable.contains(CELL)
+            ),
+            RadioInfo(
+                R.string.option_airplane_mode_radio_bluetooth,
+                BT,
+                !currentBlacklisted.contains(BT),
+                currentToggleable.contains(BT)
+            ),
+            RadioInfo(
+                R.string.option_airplane_mode_radio_wifi,
+                WIFI,
+                !currentBlacklisted.contains(WIFI),
+                currentToggleable.contains(WIFI)
+            ),
+            RadioInfo(
+                R.string.option_airplane_mode_radio_nfc,
+                NFC,
+                !currentBlacklisted.contains(NFC),
+                currentToggleable.contains(NFC)
+            ),
+            RadioInfo(
+                R.string.option_airplane_mode_radio_wimax,
+                WMX,
+                !currentBlacklisted.contains(WMX),
+                currentToggleable.contains(WMX)
+            )
+        )
 
-        radioBt.apply {
-            isChecked = !currentBlacklisted.contains(BT)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateBlacklist(radioCell.isChecked, isChecked, radioWifi.isChecked, radioNfc.isChecked, radioWmx.isChecked)
-            }
-        }
-
-        radioWifi.apply {
-            isChecked = !currentBlacklisted.contains(WIFI)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateBlacklist(radioCell.isChecked, radioBt.isChecked, isChecked, radioNfc.isChecked, radioWmx.isChecked)
-            }
-        }
-
-        radioNfc.apply {
-            isChecked = !currentBlacklisted.contains(NFC)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateBlacklist(radioCell.isChecked, radioBt.isChecked, radioWifi.isChecked, isChecked, radioWmx.isChecked)
-            }
-        }
-
-        radioWmx.apply {
-            isChecked = !currentBlacklisted.contains(WMX)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateBlacklist(radioCell.isChecked, radioBt.isChecked, radioWifi.isChecked, radioNfc.isChecked, isChecked)
-            }
-        }
-
-        toggleCell.apply {
-            isChecked = currentToggleable.contains(CELL)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateToggleable(isChecked, toggleBt.isChecked, toggleWifi.isChecked, toggleNfc.isChecked, toggleWmx.isChecked)
-            }
-        }
-
-        toggleBt.apply {
-            isChecked = currentToggleable.contains(BT)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateToggleable(toggleCell.isChecked, isChecked, toggleWifi.isChecked, toggleNfc.isChecked, toggleWmx.isChecked)
-            }
-        }
-
-        toggleWifi.apply {
-            isChecked = currentToggleable.contains(WIFI)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateToggleable(toggleCell.isChecked, toggleBt.isChecked, isChecked, toggleNfc.isChecked, toggleWmx.isChecked)
-            }
-        }
-
-        toggleNfc.apply {
-            isChecked = currentToggleable.contains(NFC)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateToggleable(toggleCell.isChecked, toggleBt.isChecked, toggleWifi.isChecked, isChecked, toggleWmx.isChecked)
-            }
-        }
-
-        toggleWmx.apply {
-            isChecked = currentToggleable.contains(WMX)
-            setOnCheckedChangeListener { _, isChecked ->
-                updateToggleable(toggleCell.isChecked, toggleBt.isChecked, toggleWifi.isChecked, toggleNfc.isChecked, isChecked)
-            }
-        }
+        val adapter = RadioAdapter(items)
+        radio_recycler.adapter = adapter
     }
-    
-    private fun updateBlacklist(isCellChecked: Boolean, isBtChecked: Boolean, isWifiChecked: Boolean, isNfcChecked: Boolean, isWmxChecked: Boolean) {
-        val list = arrayListOf<String>()
-        if (!isCellChecked) list.add(CELL)
-        if (!isBtChecked) list.add(BT)
-        if (!isWifiChecked) list.add(WIFI)
-        if (!isNfcChecked) list.add(NFC)
-        if (!isWmxChecked) list.add(WMX)
-        
-        val string = TextUtils.join(",", list)
-        
-        context.writeGlobal(Settings.Global.AIRPLANE_MODE_RADIOS, string)
-    }
-    
-    private fun updateToggleable(isCellChecked: Boolean, isBtChecked: Boolean, isWifiChecked: Boolean, isNfcChecked: Boolean, isWmxChecked: Boolean) {
-        val list = arrayListOf<String>()
-        if (isCellChecked) list.add(CELL)
-        if (isBtChecked) list.add(BT)
-        if (isWifiChecked) list.add(WIFI)
-        if (isNfcChecked) list.add(NFC)
-        if (isWmxChecked) list.add(WMX)
 
-        val string = TextUtils.join(",", list)
+    class RadioAdapter(private val items: ArrayList<RadioInfo>) : RecyclerView.Adapter<RadioAdapter.VH>() {
+        class VH(view: View) : RecyclerView.ViewHolder(view)
 
-        context.writeGlobal(Settings.Global.AIRPLANE_MODE_TOGGLEABLE_RADIOS, string)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+            return VH(LayoutInflater.from(parent.context).inflate(R.layout.airplane_mode_radio, parent, false))
+        }
+
+        override fun getItemCount(): Int {
+            return items.size
+        }
+
+        override fun onBindViewHolder(holder: VH, position: Int) {
+            holder.itemView.apply {
+                val info = items[position]
+
+                radio.text = resources.getText(info.name)
+                exempt.isChecked = info.isExempt
+                toggleable.isChecked = info.isToggleable
+
+                exempt.setOnClickListener {
+                    val newInfo = items[holder.adapterPosition]
+                    exempt.isChecked = !exempt.isChecked
+                    newInfo.isExempt = exempt.isChecked
+                    update(context)
+                }
+                toggleable.setOnClickListener {
+                    val newInfo = items[holder.adapterPosition]
+                    toggleable.isChecked = !toggleable.isChecked
+                    newInfo.isToggleable = toggleable.isChecked
+                    update(context)
+                }
+            }
+        }
+
+        private fun update(context: Context) {
+            val blacklisted = ArrayList<String>()
+            val toggleable = ArrayList<String>()
+
+            items.forEach {
+                if (!it.isExempt) blacklisted.add(it.id)
+                if (it.isToggleable) toggleable.add(it.id)
+            }
+
+            context.writeGlobal(Settings.Global.AIRPLANE_MODE_RADIOS, blacklisted.joinToString(separator = ","))
+            context.writeGlobal(Settings.Global.AIRPLANE_MODE_TOGGLEABLE_RADIOS, toggleable.joinToString(separator = ","))
+        }
     }
 }
