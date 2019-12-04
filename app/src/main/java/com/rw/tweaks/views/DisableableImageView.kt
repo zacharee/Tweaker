fcd591210d778f7ff80a7c5f083a01656a480c28
zacharee <zachary.wander@gmail.com>
2019-12-04 13:16:11 -0500

    Animate disabling/enabling DisableableImageView

--- app/src/main/java/com/rw/tweaks/views/DisableableImageView.kt
+++ app/src/main/java/com/rw/tweaks/views/DisableableImageView.kt
@@ -1,5 +1,6 @@
 package com.rw.tweaks.views
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
@@ -9,9 +10,19 @@ open class DisableableImageView : AppCompatImageView {
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    private var alphaAnim: ValueAnimator? = null
+
     override fun setEnabled(enabled: Boolean) {
         if (this.isEnabled != enabled) {
-            this.imageAlpha = if (enabled) 0xFF else 0x3F
+            alphaAnim?.cancel()
+
+            alphaAnim = ValueAnimator.ofInt(this.imageAlpha, if (enabled) 0xFF else 0x3F)
+                .apply {
+                    addUpdateListener {
+                        this@DisableableImageView.imageAlpha = it.animatedValue.toString().toInt()
+                    }
+                    start()
+                }
         }
         isClickable = enabled
         super.setEnabled(enabled)
