f27f6afb69b8cc00e2bf3d117305b7c81f17bceb
zacharee <zachary.wander@gmail.com>
2020-01-23 19:32:50 -0500

    Fix model checks

--- app/src/main/java/com/rw/tweaks/util/Utils.kt
+++ app/src/main/java/com/rw/tweaks/util/Utils.kt
@@ -81,16 +81,16 @@ val Context.isTouchWiz: Boolean
     get() = packageManager.hasSystemFeature("com.samsung.feature.samsung_experience_mobile")
 
 val isHTC: Boolean
-    get() = SystemProperties.get("ro.build.sense.version") != null
+    get() = !SystemProperties.get("ro.build.sense.version").isNullOrBlank()
 
 val isLG: Boolean
-    get() = SystemProperties.get("ro.lge.lguiversion") != null
+    get() = !SystemProperties.get("ro.lge.lguiversion").isNullOrBlank()
 
 val isHuawei: Boolean
-    get() = SystemProperties.get("ro.build.hw_emui_api_level") != null
+    get() = !SystemProperties.get("ro.build.hw_emui_api_level").isNullOrBlank()
 
 val isXiaomi: Boolean
-    get() = SystemProperties.get("ro.miui.ui.version.code") != null
+    get() = !SystemProperties.get("ro.miui.ui.version.code").isNullOrBlank()
 
 val Preference.defaultValue: Any?
     get() {
