5df2b21a5fcf281072e74b0c7e5e06004a598542
zacharee <zachary.wander@gmail.com>
2020-02-02 20:51:22 -0500

    Improve search select animation

--- app/src/main/java/com/rw/tweaks/fragments/BasePrefFragment.kt
+++ app/src/main/java/com/rw/tweaks/fragments/BasePrefFragment.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.view.animation.AnticipateInterpolator
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.animation.doOnEnd
@@ -146,6 +147,7 @@ abstract class BasePrefFragment : PreferenceFragmentCompat(), CoroutineScope by 
                         anim.repeatCount = 1
                         anim.repeatMode = ValueAnimator.REVERSE
                         anim.duration = time
+                        anim.interpolator = AnticipateInterpolator()
                         anim.doOnEnd {
                             item.isPressed = false
                         }
