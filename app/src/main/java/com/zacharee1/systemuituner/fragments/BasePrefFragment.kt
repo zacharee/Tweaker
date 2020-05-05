56ae727535b4afbe9414aa9b7f58471e07b4c101
Zachary Wander <zachary.wander@gmail.com>
2020-05-04 21:53:47 -0400

    Remove unused import

--- app/src/main/java/com/zacharee1/systemuituner/fragments/BasePrefFragment.kt
+++ app/src/main/java/com/zacharee1/systemuituner/fragments/BasePrefFragment.kt
@@ -1,6 +1,5 @@
 package com.zacharee1.systemuituner.fragments
 
-import android.animation.LayoutTransition
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import android.animation.ValueAnimator
