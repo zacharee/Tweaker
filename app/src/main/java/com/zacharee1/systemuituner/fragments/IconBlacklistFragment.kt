904a16eb440c39fcabc1543269fe6589f4ea2aa1
zacharee <zachary.wander@gmail.com>
2021-02-12 23:55:40 -0500

    Remove unused import

--- app/src/main/java/com/zacharee1/systemuituner/fragments/IconBlacklistFragment.kt
+++ app/src/main/java/com/zacharee1/systemuituner/fragments/IconBlacklistFragment.kt
@@ -3,7 +3,6 @@ package com.zacharee1.systemuituner.fragments
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.Configuration
