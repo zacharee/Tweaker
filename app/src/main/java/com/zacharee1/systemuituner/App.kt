9e5aaa33991031a89220700362cd6db2a1d5e191
Zachary Wander <zachary.wander@gmail.com>
2022-06-16 12:37:17 -0400

    Try using `bindService` instead of `startForegroundService`

--- app/src/main/java/com/zacharee1/systemuituner/App.kt
+++ app/src/main/java/com/zacharee1/systemuituner/App.kt
@@ -1,12 +1,12 @@
 package com.zacharee1.systemuituner
 
 import android.app.Application
-import android.content.Context
-import android.content.Intent
-import android.content.SharedPreferences
+import android.content.*
 import android.os.Build
+import android.os.IBinder
+import android.os.Looper
+import android.util.AndroidRuntimeException
 import android.util.Log
-import androidx.core.content.ContextCompat
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.zacharee1.systemuituner.services.Manager
 import com.zacharee1.systemuituner.util.PersistenceHandlerRegistry
@@ -16,13 +16,21 @@ import org.lsposed.hiddenapibypass.HiddenApiBypass
 
 class App : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
     companion object {
+        private val connection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {}
+            override fun onServiceDisconnected(name: ComponentName?) {}
+        }
+
         fun updateServiceState(context: Context) {
             with (context) {
                 if (prefManager.persistentOptions.isEmpty()) {
+                    try {
+                        unbindService(connection)
+                    } catch (_: IllegalArgumentException) {}
                     stopService(Intent(this, Manager::class.java))
                 } else {
                     try {
-                        ContextCompat.startForegroundService(this, Intent(this, Manager::class.java))
+                        bindService(Intent(this, Manager::class.java), connection, Context.BIND_AUTO_CREATE)
                     } catch (e: Exception) {
                         Log.e("SystemUITuner", "Unable to start foreground service. Build SDK ${Build.VERSION.SDK_INT}.", e)
                         FirebaseCrashlytics.getInstance().apply {
@@ -36,6 +44,8 @@ class App : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
 
     override fun onCreate() {
         super.onCreate()
+
+        initExceptionHandler()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             HiddenApiBypass.setHiddenApiExemptions("L")
@@ -51,6 +61,20 @@ class App : Application(), SharedPreferences.OnSharedPreferenceChangeListener {
         when (key) {
             PrefManager.PERSISTENT_OPTIONS -> {
                 updateServiceState(this)
+            }
+        }
+    }
+
+    private fun initExceptionHandler() {
+        val previousHandler = Thread.getDefaultUncaughtExceptionHandler()
+
+        Thread.setDefaultUncaughtExceptionHandler { t, e ->
+            if (e is AndroidRuntimeException) {
+                Log.e("SystemUITuner", "Caught a runtime Exception!", e)
+                FirebaseCrashlytics.getInstance().recordException(Exception("Caught a runtime Exception!", e))
+                Looper.loop()
+            } else {
+                previousHandler?.uncaughtException(t, e)
             }
         }
     }
